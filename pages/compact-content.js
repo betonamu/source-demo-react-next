1b0e6f9ef25af0f17e27f13e5ddf6e8f57f3c7f1
betonamu <nguyencongquoc0904@gmail.com>
2021-08-24 15:28:54 +0700

    compact content component

---
+++ pages/compact-content.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CompactContent from "../components/CompactContent";
+
+const CompactContentPage = () =>{
+    return(
+        <CompactContent/>
+    )
+}
+
+export default CompactContentPage;
