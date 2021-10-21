2fbddf41838d3429bdd844da67a67663cece2bfd
hoangkhoa2k <khoanguyen.491449@gmail.com>
2021-10-21 10:28:07 +0700

    update-product

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from "redux";
 
 import home from "./home";
 import main from "./main";
+import product from "./product";
 
 const rootReducer = combineReducers({
     home,
     main,
+    product,
 });
 
 export default rootReducer;
